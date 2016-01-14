9373d0655b3622bc646f6d9fdeacacfe94719c0f
Michael Hobbs <michael.lee.hobbs@gmail.com>
2016-01-14 06:42:20 -0500

    fixed restart on failure buttons

--- client/app/servermanagement/servermanagement.controller.js
+++ client/app/servermanagement/servermanagement.controller.js
@@ -9,7 +9,6 @@
       this.$http = $http;
       this.$scope = $scope;
 
-      this.servers = [];
       this.serverAddons = [];
       this.serverKeys = [];
       this.mcKeys = [];
@@ -19,7 +18,13 @@
       this.edittingServer = undefined;
       this.edittingItem = undefined;
 
-      this.btnData = {};
+      this.btns = {
+        data: {
+          editing: false,
+          editingServer: undefined,
+        }
+      };
+
 
       this.activePanelModel = -1;
 
@@ -28,6 +33,7 @@
       this.getCurrentUser = Auth.getCurrentUser;
 
       $http.get('/api/servers').then(response => {
+        console.log(response.data);
         var servers = response.data;
 
         servers.forEach(function (server) {
@@ -60,166 +66,6 @@
       });
     }
 
-    injectButtons(server) {
-      var self = this;
-      console.log(self);
-      server.btn = {};
-      // a space for buttons to share data
-      server.btn.data = {};
-      server.btn.startServer = {
-        disabled: false,
-        onClick: function () {
-          var self = this;
-          console.log('start server');
-          self.$http.put('/api/servers/' + server._id + '/start');
-          // disable the buttons for 60 seconds to prevent the user from spaming the server with stop/restart
-          server.btn.stopServer.disabled = true;
-          server.btn.startServer.disabled = true;
-          _.delay(()=> {self.server.btn.stopServer.disabled = false;}, 60000);
-        } // end func
-      }; // end server.btn.startServer
-      server.btn.stopServer = {
-        disabled: false,
-        onClick: function () {
-          console.log('stop server');
-          self.$http.put('/api/servers/' + server._id + '/stop');
-          // disable the buttons for 60 seconds to prevent the user from spaming the server with stop/restart
-          self.btn.stopServer.disabled = true;
-          self.btn.startServer.disabled = true;
-          _.delay(()=> {self.server.btn.startServer.disabled = false;}, 60000);
-        }
-      }; // end server.btn.stopServer
-      server.btn.edit = {
-        disabled: false,
-        onClick: function (what) {
-          // save uneditable items so that we can restore them
-          self.btnData[server._id] = {
-            name: server.name,
-            info: server.info,
-            visibility: server.visibility,
-            shouldRestart: server.shouldRestart,
-            msmProp: _.clone(server.msmProp),
-            mineCraftProp: _.clone(server.mineCraftProp)
-          };
-          server.btn.edit.disabled = true;
-          server.btn.cancel.disabled = false;
-          server.btn.save.disabled = false;
-          self.isEditting = true;
-          self.edittingServer = server._id;
-          self.edittingItem = what;
-        } // end func
-      }; // end server.btn.edit
-      server.btn.cancel = {
-        disabled: true,
-        onClick: function () {
-          // restore unedit server data
-          console.log(self.btnData[server._id].mineCraftProp);
-          _.merge(server, self.btnData[server._id]);
-          _.merge(server.msmProp, self.btnData[server._id].msmProp);
-          _.merge(server.mineCraftProp, self.btnData[server._id].mineCraftProp);
-          delete self.btnData[server._id];
-          server.btn.edit.disabled = false;
-          server.btn.cancel.disabled = true;
-          server.btn.save.disabled = true;
-          self.isEditting = false;
-          self.edittingServer = undefined;
-          self.edittingItem = undefined;
-        } // end func
-      }; // end server.btn.cancel
-      server.btn.save = {
-        disabled: true,
-        onClick: function () {
-
-          switch (self.edittingItem) {
-            case 'settings':
-              self.updateSettings(server);
-              break;
-            case 'messages':
-              self.updateMessages(server);
-              break;
-            case 'properties':
-              self.updateProperties(server);
-              break;
-            case 'msm':
-              self.updateMsm(server);
-              break;
-          }// end switch
-          server.btn.edit.disabled = false;
-          server.btn.cancel.disabled = true;
-          server.btn.save.disabled = true;
-          self.isEditting = false;
-          self.edittingServer = undefined;
-          self.edittingItem = undefined;
-        } // end function
-      }; // end server.btn.save
-      server.btn.host = {
-        disabled: false,
-        onClick: function () {
-          // todo impliment host button
-        } // end func
-      }; // end server.btn.host
-      server.btn.port = {
-        disabled: false,
-        onClick: function () {
-          // todo impliment port button
-        } // end func
-      }; // end server.btn.port
-      console.log("test:");
-      console.log(server.btn)
-    };
-
-    updateSettings(server) {
-      console.log(this);
-      let update = {
-        name: server.name,
-        info: server.info,
-        visibility: server.visibility,
-        shouldRestart: server.shouldRestart,
-        msmProp: {
-          'msm-stop-delay': server.msmProp['msm-stop-delay'],
-          'msm-restart-delay': server.msmProp['msm-restart-delay']
-        } // end msmProp
-      };// end update
-      this.updateServer(server, update);
-    }
-
-    updateMessages(server) {
-      let update = {
-        msmProp: {
-          'msm-message-stop': server.msmProp['msm-message-stop'],
-          'msm-message-stop-abort': server.msmProp['msm-message-stop-abort'],
-          'msm-message-restart': server.msmProp['msm-message-restart'],
-          'msm-message-restart-abort': server.msmProp['msm-message-restart-abort'],
-          'msm-message-world-backup-started': server.msmProp['msm-message-world-backup-started'],
-          'msm-message-world-backup-finished': server.msmProp['msm-message-world-backup-finished'],
-          'msm-message-complete-backup-started': server.msmProp['msm-message-complete-backup-started'],
-          'msm-message-complete-backup-finished': server.msmProp['msm-message-complete-backup-finished']
-        } // end msmProp
-      }; // end update
-      updatedServer(server, update);
-    }
-
-    updateProperties(server) {
-      let update = {
-        mineCraftProp: server.mineCraftProp
-      };
-      update.mineCraftProp.rcon = {};
-      update.mineCraftProp.rcon.password = server.mineCraftProp.rconPassword;
-      delete update.mineCraftProp.rconPassword;
-      delete update.mineCraftProp.rconPort;
-      delete update.mineCraftProp.queryPort;
-      delete update.mineCraftProp['server-port'];
-      updatedServer(server, update);
-    }
-
-    updateMsm(server) {
-      let update = {
-        msmProp: server.msmProp
-      };
-      updatedServer(server, update);
-    }
-
-
     cleanProperty(prop) {
       prop = prop.replace('msm-', '');
       prop = prop.replace('message', 'msg');
@@ -234,12 +80,17 @@
       }
     }
 
-    updateServer(server, update) {
-      if (update !== undefined) {
-        this.$http.put('/api/servers/' + server._id, update);
-        return;
-      }
-      this.$http.put('/api/servers/' + server._id, server);
+    updateServer(server) {
+      var update = _.clone(server, true);
+      update.mineCraftProp.rcon = {};
+      update.mineCraftProp.rcon.password = server.mineCraftProp.rconPassword;
+      delete update.mineCraftProp.rconPassword;
+      delete update.mineCraftProp.rconPort;
+      delete update.mineCraftProp.queryPort;
+      delete update.mineCraftProp['server-port'];
+      delete update.btn;
+
+      this.$http.put('/api/servers/' + server._id, update);
     }
 
     deleteServer(server) {
@@ -252,6 +103,151 @@
           console.log('result');
           console.log(result);
         })
+    }
+
+    injectButtons(server) {
+      let self = this;
+      console.log(self)
+      console.log("injecting buttons into " + server._id);
+      let srvrId = server._id;
+      this.btns.data[srvrId] = {
+        startServer: { disabled: false },
+        stopServer: { disabled: false },
+        edit: { disabled: false },
+        cancel: { disabled: true },
+        save: { disabled: true },
+        host: { disabled: false },
+        port: { disabled: false },
+        restartOnFailure: { disabled: false },
+      };
+      console.log(this.btns.data);
+
+      server.btn = {
+        startServer: {
+          onClick() {
+            self.$http.put('/api/servers/' + srvrId + '/start');
+            self.btns.data[srvrId].startServer.disabled = true;
+            self.btns.data[srvrId].stopServer.disabled = true;
+
+            _.delay(()=> {
+              self.btns.data[srvrId].stopServer.disabled = false;
+            }, 60000);
+          },
+          isDisabled() {
+            return self.btns.data[srvrId].startServer.disabled;
+          }
+        },
+        stopServer: {
+          onClick() {
+            self.$http.put('/api/servers/' + srvrId + '/stop');
+            self.btns.data[srvrId].startServer.disabled = true;
+            self.btns.data[srvrId].stopServer.disabled = true;
+
+
+            _.delay(()=> {
+              self.btns.data[srvrId].startServer.disabled = false;
+            }, 60000);
+          },
+          isDisabled() {
+            return self.btns.data[srvrId].stopServer.disabled;
+          }
+        },
+        edit: {
+          onClick() {
+            // save unedited items so that we can restore them
+            let server = _.find(self.servers, {'_id': srvrId});
+            self.btns.data[srvrId].data = {
+              name: server.name,
+              info: server.info,
+              visibility: server.visibility,
+              shouldRestart: server.shouldRestart,
+              msmProp: _.clone(server.msmProp),
+              mineCraftProp: _.clone(server.mineCraftProp)
+            };
+            self.btns.data[srvrId].edit.disabled = true;
+            self.btns.data[srvrId].cancel.disabled = false;
+            self.btns.data[srvrId].save.disabled = false;
+            self.btns.data.isEditting = true;
+            self.btns.data.edittingServer = server._id;
+          },
+          isDisabled() {
+            return self.btns.data[srvrId].edit.disabled;
+          }
+        },
+        cancel: {
+          onClick() {
+            // restore unedit server data
+            let server = _.find(self.servers, {'_id': srvrId});
+            _.merge(server, self.btns.data[srvrId].data);
+            _.merge(server.msmProp, self.btns.data[srvrId].data.msmProp);
+            _.merge(server.mineCraftProp, self.btns.data[srvrId].data.mineCraftProp);
+            delete self.btns.data[srvrId].data;
+            self.btns.data[srvrId].edit.disabled = false;
+            self.btns.data[srvrId].cancel.disabled = true;
+            self.btns.data[srvrId].save.disabled = true;
+            self.btns.data.isEditting = false;
+            self.btns.data.edittingServer = undefined;
+          },
+          isDisabled(){
+            return self.btns.data[srvrId].cancel.disabled;
+          }
+        },
+        save: {
+          onClick() {
+            let server = _.find(self.servers, {'_id': srvrId});
+            self.updateServer(server);
+            self.btns.data[srvrId].edit.disabled = false;
+            self.btns.data[srvrId].cancel.disabled = true;
+            self.btns.data[srvrId].save.disabled = true;
+            self.btns.data.isEditting = false;
+            self.btns.data.edittingServer = undefined;
+            //self.$scope().$apply();
+          },
+          isDisabled() {
+            return self.btns.data[srvrId].save.disabled;
+          }
+        },
+        host: {
+          onClick(){
+
+          },
+          isDisabled(){
+            return self.btns.data[srvrId].host.disabled;
+          }
+        },
+        port: {
+          onClick(){
+
+          },
+          isDisabled(){
+            return self.btns.data[srvrId].host.disabled;
+          }
+        },
+        restartOnFailure: {
+          get: function() {
+            return server.shouldRestart;
+          },
+          onClick(boolean){
+            if (!self.btns.data.isEditting || self.btns.data.edittingServer !== server._id) {
+              return server.shouldRestart;
+            }
+            return server.shouldRestart = boolean;
+          },
+          isDisabled(){
+            return self.btns.data[srvrId].restartOnFailure.disabled;
+          },
+          style(){
+            if (!self.btns.data.isEditting || self.btns.data.edittingServer != server._id) {
+              return {
+                "cursor": "not-allowed",
+                "pointer-events": "none !important",
+                "background-color": "#EEEEEE"
+              }
+            }
+            return {}
+          }
+        }
+      };
     }
   }
 
